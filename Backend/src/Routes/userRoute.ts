255dced140bb2660186fafb91c618c7c88e7cbca
Himanshu Singh <himanshujrs6@gmail.com>
2024-09-11 00:24:04 +0530

    commit-2nd-11-9

--- Backend/src/Routes/userRoute.ts
+++ Backend/src/Routes/userRoute.ts
@@ -1,9 +1,15 @@
-import  express  from "express";
-import{jwtCheck,jwtParse} from "../middleware/auth"
+import express from "express";
+import { jwtCheck, jwtParse } from "../middleware/auth"
 import MyUserController from "../Controller/userController";
-const router=express.Router();
+import { validateMyUserRequest } from '../middleware/validation';
+const router = express.Router();
 
-router.post('/',jwtCheck,MyUserController.createCurrentUser);
-router.put('/',jwtCheck,jwtParse,MyUserController.updateCurrentUser);
+router.post('/', jwtCheck, MyUserController.createCurrentUser);
+router.put('/', 
+    jwtCheck, 
+    jwtParse, 
+    validateMyUserRequest, 
+    MyUserController.updateCurrentUser
+);
 // router.get('/',userController.createCurrentUser);
 export default router;
